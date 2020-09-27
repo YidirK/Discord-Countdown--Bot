7b94ad8495a67da88d0a288d25ceb941760b8367
yidirkahlouche <31509703+yidirm@users.noreply.github.com>
2020-09-27 15:55:31 +0100

    6.0

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
-import discord, json, time
+import discord, json, time, os, random
 
 with open("config.json") as _j:
 	__token = json.loads(_j.read())["token"]
 
 __client = discord.Client()
 __timer_max = 120
-
+__num_emoji = [":zero:", ":one:", ":two:", ":three:", ":four:",
+					 ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":ten:"]
 
 @__client.event
 async def on_ready():
@@ -39,14 +40,33 @@ async def cmd_timer(bot_message):
 		t = int(_args[0])
 		if t > __timer_max:
 			await bot_message.channel.send("impossible! (!help)")
+			if t == 10:
+				await bot_message.channel.send(":one::zero:")
+				return
+			if t == 9:
+				await bot_message.channel.send(":nine:")
+				return
 		else:
 			while t:
 				time.sleep(1)
 				t -= 1
-			await bot_message.channel.send("Time's up!")
+			await bot_message.channel.send(f"Time's up! {_args[0]}s,{_args[1]} ")
 	else:
 		# donc l'utilisateur n'a pas mis d'arguments
 		await bot_message.channel.send("Aw, t'as pas mis d'arguments a winnat !")
+
+		@__client.event
+		async def on_message(message: discord.Message):
+			if message.author == __client.user:
+				return
+			if message.type != discord.MessageType.default:
+				return
+			if message.content[0] == '!':
+				_msg = message.content.split()  # ça permet de découper le message là où il y a des espaces
+				if _msg[0] == '!help':
+					await bot_message.channel.send("test")
+
+
 
 
 __client.run(__token)
