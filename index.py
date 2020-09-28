3ee8d95a135abad2456f7827918b973483ca995f
yidirkahlouche <31509703+yidirm@users.noreply.github.com>
2020-09-28 14:20:03 +0100

    2.0

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 import discord, json, time, os, random
+=======
+import discord, json, time
+>>>>>>> Stashed changes
 
 with open("config.json") as _j:
 	__token = json.loads(_j.read())["token"]
@@ -7,6 +11,7 @@ __client = discord.Client()
 __timer_max = 120
 __num_emoji = [":zero:", ":one:", ":two:", ":three:", ":four:",
 					 ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":ten:"]
+
 
 @__client.event
 async def on_ready():
@@ -17,9 +22,23 @@ async def on_ready():
 async def on_message(message: discord.Message):
 	if message.author == __client.user:
 		return
+<<<<<<< Updated upstream
 
 	if message.type != discord.MessageType.default:
 		return
+=======
+
+	if message.type != discord.MessageType.default:
+		return
+
+	if message.content[0] == '!':
+		# donc dans le cas où le message commence par !, c'est une commande
+		# On va voir si c'est la commande qui nous intéresse
+		_msg = message.content.split()  # ça permet de découper le message là où il y a des espaces
+		if _msg[0] == '!timer':
+			# donc le message commence par !timer suivi d'une espace
+			await cmd_timer(message)
+>>>>>>> Stashed changes
 
 	if message.content[0] == '!':
 		# donc dans le cas où le message commence par !, c'est une commande
@@ -38,6 +57,7 @@ async def cmd_timer(bot_message):
 		# on sait désormais que l'utilisateur veut utiliser !timer avec les arguments listés dans _args
 		await bot_message.channel.send("Tu veux !timer avec les arguments %s" % _args)
 		t = int(_args[0])
+<<<<<<< Updated upstream
 		if t > __timer_max:
 			await bot_message.channel.send("impossible! (!help)")
 			if t == 10:
@@ -51,10 +71,17 @@ async def cmd_timer(bot_message):
 				time.sleep(1)
 				t -= 1
 			await bot_message.channel.send(f"Time's up! {_args[0]}s,{_args[1]} ")
+=======
+		while t:
+			time.sleep(1)
+			t -= 1
+		await bot_message.channel.send("Time's up!")
+>>>>>>> Stashed changes
 	else:
 		# donc l'utilisateur n'a pas mis d'arguments
 		await bot_message.channel.send("Aw, t'as pas mis d'arguments a winnat !")
 
+<<<<<<< Updated upstream
 		@__client.event
 		async def on_message(message: discord.Message):
 			if message.author == __client.user:
@@ -68,5 +95,7 @@ async def cmd_timer(bot_message):
 
 
 
+=======
+>>>>>>> Stashed changes
 
 __client.run(__token)
